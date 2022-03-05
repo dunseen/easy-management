e19daf2bc99730f480c084b9a656438b996d5d04
davys.lima-extern@renault.com <davys.lima-extern@renault.com>
2022-03-05 01:40:18 -0300

    feat: create order 1/2

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -6,6 +6,11 @@ import {
     FormLabel,
     Input as ChakraInput,
     InputProps as ChakraInputProps,
+    NumberDecrementStepper,
+    NumberIncrementStepper,
+    NumberInput,
+    NumberInputField,
+    NumberInputStepper,
 } from '@chakra-ui/react';
 
 import { FieldError } from 'react-hook-form';
@@ -15,24 +20,53 @@ interface InputProps extends ChakraInputProps {
     label?: string;
     error?: FieldError;
     ref?: any;
+    inputType?: string;
 }
 
-export function Input({ error = null, name, label, ...rest }: InputProps) {
+export function Input({
+    error = null,
+    name,
+    label,
+    inputType,
+    ...rest
+}: InputProps) {
     return (
         <FormControl isInvalid={!!error}>
             {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
-            <ChakraInput
-                name={name}
-                id={name}
-                focusBorderColor='pink.500'
-                bg='gray.900'
-                variant='filled'
-                _hover={{
-                    bg: 'gray.900',
-                }}
-                size='lg'
-                {...rest}
-            />
+            {inputType === 'numberQuantity' ? (
+                <NumberInput
+                    name={name}
+                    id={name}
+                    focusBorderColor='pink.500'
+                    bg='gray.900'
+                    variant='outline'
+                    _hover={{
+                        bg: 'gray.900',
+                    }}
+                    min={0}
+                    size='lg'
+                    defaultValue={1}
+                >
+                    <NumberInputField />
+                    <NumberInputStepper>
+                        <NumberIncrementStepper />
+                        <NumberDecrementStepper />
+                    </NumberInputStepper>
+                </NumberInput>
+            ) : (
+                <ChakraInput
+                    name={name}
+                    id={name}
+                    focusBorderColor='pink.500'
+                    bg='gray.900'
+                    variant='filled'
+                    _hover={{
+                        bg: 'gray.900',
+                    }}
+                    size='lg'
+                    {...rest}
+                />
+            )}
 
             {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
         </FormControl>
--- src/pages/Orders/Details.tsx
+++ src/pages/Orders/Details.tsx
@@ -1,112 +1,241 @@
 import {
     Box,
     Button,
+    Divider,
     Flex,
+    FormControl,
+    FormLabel,
+    Heading,
     HStack,
     Icon,
+    NumberDecrementStepper,
+    NumberIncrementStepper,
+    NumberInput,
+    NumberInputField,
+    NumberInputStepper,
     Select,
     SimpleGrid,
+    Tag,
+    TagLabel,
+    TagRightIcon,
     Text,
     VStack,
 } from '@chakra-ui/react';
 import React from 'react';
-import { RiPencilLine, RiDeleteBin2Line } from 'react-icons/ri';
+import { RiPencilLine, RiDeleteBin2Line, RiAddLine } from 'react-icons/ri';
 import { Input } from '../../components/Input';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+
+interface AddressFormData {
+    street: string;
+    zipCode: string;
+    complement: string;
+    neighborhood: string;
+    city: string;
+    state: string;
+}
+
+interface CreteUserFormData {
+    name: string;
+    fantasyName: string;
+    document: string;
+    email: string;
+    contact: string;
+    address: AddressFormData;
+    factory: string;
+    fee: string;
+}
+
+const createUserSchema = yup.object().shape({
+    name: yup.string().required('Razão Social obrigatória.'),
+    fantasyName: yup.string().required('Nome Fantasia obrigatório.'),
+    document: yup.string().required('Documento obrigatório.'),
+    contact: yup.string().required('Contato obrigatório.'),
+    email: yup
+        .string()
+        .required('E-mail obrigatório.')
+        .email('E-mail inválido.'),
+    zipCode: yup.string().required('CEP obrigatório.'),
+    street: yup.string().required('Rua obrigatória.'),
+    neighborhood: yup.string().required('Bairro obrigatório.'),
+    city: yup.string().required('Cidade obrigatório.'),
+    state: yup.string().required('Estado obrigatório.'),
+    factory: yup.string().required('Fábrica obrigatória.'),
+    fee: yup.string().required('Frete obrigatório.'),
+});
 
 const OrderDetails = () => {
+    const { register, formState, handleSubmit } = useForm({
+        resolver: yupResolver(createUserSchema),
+    });
+
+    const { errors } = formState;
+
+    const handleUserFormSubmit: SubmitHandler<CreteUserFormData> = async (
+        formData
+    ) => {
+        console.log(formData);
+    };
     return (
-        <Flex
-            w='100%'
-            padding={['8', '16px']}
-            gap='4'
-            bgColor={'gray.700'}
-            border='1px'
-        >
-            <Flex
-                w='100%'
-                justifyContent={'center'}
-                alignItems='center'
-                direction='column'
+        <Flex w='100%' maxW={1480} mx='auto'>
+            <Box
+                as='form'
+                onSubmit={handleSubmit(handleUserFormSubmit)}
+                flex='1'
+                borderRadius={8}
+                bg='gray.800'
+                p={['6', '8']}
             >
-                <Flex
-                    justifyContent='space-between'
-                    alignItems={'center'}
-                    w='100%'
-                    height={'fit-content'}
-                >
-                    <Text fontSize={'lg'}>0808</Text>
-                    <Select w='fit-content'>
-                        <option value='sent'>Enviado</option>
-                        <option value='pending'>Análise</option>
-                        <option value='done'>Faturado</option>
-                        <option value='cancelled'>Cancelado</option>
-                    </Select>
+                <HStack justifyContent={'space-between'}>
+                    <HStack gap='4'>
+                        <Heading size='lg' fontWeight='bold'>
+                            Pedido: 018290
+                        </Heading>
+                        <HStack>
+                            <Heading size='md' fontWeight='normal'>
+                                Situação:
+                            </Heading>
+                            <Select w='fit-content'>
+                                <option value='sent'>Enviado</option>
+                                <option value='pending'>Análise</option>
+                                <option value='done'>Faturado</option>
+                                <option value='cancelled'>Cancelado</option>
+                            </Select>
+                        </HStack>
+                    </HStack>
+                    <Button
+                        type='button'
+                        colorScheme='pink'
+                        rightIcon={<Icon as={RiAddLine} fontSize='20' />}
+                    >
+                        Adicionar produtos
+                    </Button>
+                </HStack>
+
+                <Divider my='6' borderColor='gray.700' />
+
+                <Flex mt='8' gap='8'>
+                    <VStack spacing='8'>
+                        <SimpleGrid
+                            minChildWidth='320px'
+                            spacing={['6', '8']}
+                            w='100%'
+                        >
+                            <Input
+                                error={errors.document}
+                                name='document'
+                                label='CNPJ'
+                                {...register('document')}
+                            />
+                            <Input
+                                error={errors.fantasyName}
+                                name='fantasyName'
+                                label='Nome Fantasia'
+                                {...register('fantasyName')}
+                            />
+                            <Input
+                                error={errors.name}
+                                name='name'
+                                label='Razão Social'
+                                {...register('name')}
+                            />
+                            <Input
+                                error={errors.email}
+                                name='email'
+                                type='email'
+                                label='E-mail'
+                                {...register('email')}
+                            />
+                            <Input
+                                error={errors.contact}
+                                name='contact'
+                                label='Contato'
+                                {...register('contact')}
+                            />
+                        </SimpleGrid>
+                    </VStack>
+
+                    <VStack spacing='8'>
+                        <SimpleGrid
+                            minChildWidth='320px'
+                            spacing={['6', '8']}
+                            w='100%'
+                        >
+                            <Input
+                                error={errors.zipCode}
+                                name='zipCode'
+                                label='CEP'
+                                {...register('zipCode')}
+                            />
+                            <Input
+                                error={errors.city}
+                                name='city'
+                                label='Cidade'
+                                {...register('city')}
+                            />
+
+                            <Input
+                                error={errors.neighborhood}
+                                name='neighborhood'
+                                label='Bairro'
+                                {...register('neighborhood')}
+                            />
+                            <Input
+                                error={errors.street}
+                                name='street'
+                                label='Rua'
+                                {...register('street')}
+                            />
+
+                            <Input
+                                error={errors.state}
+                                name='state'
+                                label='Estado'
+                                {...register('state')}
+                            />
+                        </SimpleGrid>
+                    </VStack>
+                    <VStack spacing='8'>
+                        <SimpleGrid
+                            minChildWidth='320px'
+                            spacing={['6', '8']}
+                            w='100%'
+                        >
+                            <Input
+                                error={errors.factory}
+                                name='factory'
+                                label='Fábrica'
+                                {...register('factory')}
+                            />
+                            <Input
+                                error={errors.fee}
+                                name='fee'
+                                label='Frete'
+                                {...register('fee')}
+                            />
+                        </SimpleGrid>
+                    </VStack>
                 </Flex>
 
-                <HStack w='100%'>
-                    <SimpleGrid spacing={['6', '8']} w='100%'>
-                        <Input
-                            // error={errors.referencia}
-                            name='document'
-                            label='CNPJ'
-                            // {...register('referencia')}
-                        />
-                        <Input
-                            // error={errors.color}
-                            name='factory'
-                            label='Fábrica'
-                            // {...register('color')}
-                        />
-                        <Input
-                            // error={errors.color}
-                            name='frete'
-                            label='Frete'
-                            // {...register('color')}
-                        />
-                    </SimpleGrid>
-                </HStack>
-            </Flex>
-
-            <Box border='1px' w='100%'>
-                <Text textAlign={'center'} fontWeight={'bold'}>
-                    Produtos
-                </Text>
-                <Flex
-                    justifyContent='flex-end'
-                    alignItems={'flex-end'}
-                    bg='gray.800'
-                    gap={'8'}
-                    w='100%'
-                    border={'1px'}
-                >
-                    <Box>
-                        <Text fontWeight={'bold'}>Lavadora eletrolux</Text>
-                        <Text fontSize={'sm'} color='gray.300'>
-                            black
-                        </Text>
-                        <Text color='gray.300'>10 kg / 4 pe;as</Text>
-                    </Box>
-                    <Text fontWeight={'bold'} fontSize={'lg'}>
-                        R$ 999.999.999,00
-                    </Text>
-                    <VStack>
+                <Flex mt='8' justify='flex-end'>
+                    <HStack spacing='4'>
+                        <Link to='/dashboard/customers'>
+                            <Button as='a' colorScheme='whiteAlpha'>
+                                Cancelar
+                            </Button>
+                        </Link>
                         <Button
-                            as='a'
-                            size='sm'
-                            fontSize='sm'
+                            type='submit'
                             colorScheme='pink'
-                            leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
-                        />
-                        <Button
-                            as='a'
-                            size='sm'
-                            fontSize='sm'
-                            colorScheme='gray'
-                            leftIcon={
-                                <Icon as={RiDeleteBin2Line} fontSize='16' />
-                            }
-                        />
-                    </VStack>
+                            isLoading={formState.isSubmitting}
+                        >
+                            Salvar
+                        </Button>
+                    </HStack>
                 </Flex>
             </Box>
         </Flex>
